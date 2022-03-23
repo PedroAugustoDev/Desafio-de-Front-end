e782ff9f7b5b0532999486a22ddf9dc0be58f4ad
PedroAugustoDev <74946703+PedroAugustoDev@users.noreply.github.com>
2022-03-23 20:38:19 -0300

    update files

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/* 
+ * Autor: Pedro Augusto
+ */
+
+
+
+ //set mobile menu
+ window.addEventListener('load', ()=>{
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navBarLinks = document.querySelector('.nav');
+
+  menuToggle.addEventListener('click', () =>{
+    const header = document.querySelectorAll('header')[0];
+     menuToggle.classList.toggle('active');
+     header.classList.toggle('active')
+     navBarLinks.classList.toggle('active');
+     navBarLinks.addEventListener('click', () =>{
+         setTimeout(() => {
+            if(menuToggle.classList.contains('active'))
+              menuToggle.classList.remove('active');
+            navBarLinks.classList.remove('active');
+         }, 300);
+     });
+  });
+});
